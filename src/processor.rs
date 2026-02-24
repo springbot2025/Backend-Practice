4326eed734f684bdfb264f4847b388dd4de99c5d
springbot2025 <2922562941@qq.com>
2026-02-25 02:37:47 +0800

    new3

--- src/processor.rs
+++ src/processor.rs
@@ -6,5 +6,5 @@ pub async fn process_coffee(order : &Order) {
     println!("请等待制作完成……");
     println!("预计制作需要{}秒", time);
     sleep(Duration::from_secs(time)).await;
-    println!("咖啡制作完成！请取餐");
+    println!("{}制作完成！请取餐", order.typ.name());
 }
--- src/storage.rs
+++ src/storage.rs
@@ -4,7 +4,6 @@ use tokio::fs;
 pub async fn save_order(order: Order) {
     let json = serde_json::to_string_pretty(&order).unwrap();
     fs::create_dir_all("orders").await.unwrap();
-    let safe_name = sanitize_filename(&order.name);
-    let file_path = format!("orders/order-{}-{}.json", order.id, safe_name);
+    let file_path = format!("orders/order_{}.json", order.id);
     fs::write(file_path, json).await.unwrap();
 }
